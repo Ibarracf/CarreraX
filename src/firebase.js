dc21d43069ddc040483ebef71bcdc1dfedc4fb13
Ibarracf <110525669+Ibarracf@users.noreply.github.com>
2025-11-23 10:25:45 +0000

    cambio2

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// ESTE OBJETO DEBE CONTENER TUS CLAVES DE LA CONSOLA DE FIREBASE
+// Credenciales proporcionadas por el usuario.
 const firebaseConfig = {
-      apiKey: "AIzaSyAT4JmFe64rHe23Rsv0heE37IqVhf95AVI",
-        authDomain: "carrerasx-7fec8.firebaseapp.com",
-          projectId: "carrerasx-7fec8",
-            storageBucket: "carrerasx-7fec8.firebasestorage.app",
-              messagingSenderId: "80002686454",
-                appId: "1:80002686454:web:1b154a10cf63b5d071fbd7",
-                  measurementId: "G-6F3DP6SBDD"
-                  };
-}
+  apiKey: "AIzaSyAT4JmFe64rHe23Rsv0heE37IqVhf95AVI",
+    authDomain: "carrerasx-7fec8.firebaseapp.com",
+      projectId: "carrerasx-7fec8",
+        storageBucket: "carrerasx-7fec8.firebasestorage.app",
+          messagingSenderId: "80002686454",
+            appId: "1:80002686454:web:1b154a10cf63b5d071fbd7",
+              measurementId: "G-6F3DP6SBDD"
+              };
 
-            const app = initializeApp(firebaseConfig);
-            export const auth = getAuth(app);
-            export const db = getFirestore(app);
+              // Inicializa la aplicación de Firebase
+              const app = initializeApp(firebaseConfig);
+
+              // Exporta los servicios que usará la aplicación
+              export const auth = getAuth(app);
+              export const db = getFirestore(app);
